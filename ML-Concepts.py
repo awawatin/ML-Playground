4126a2f21c5180a3035d85745a36b8f12406c37f
Awawa <awawa>
2024-10-02 16:47:40 -0500

    Kmeans added

--- ML-Concepts.py
+++ ML-Concepts.py
@@ -22,10 +22,27 @@
 
 
 # ================================================ K-Means==================================================================================
-# Purpose:
-# Intuition:
-# Type:
-# Application Areas:
+# Purpose: Grouping unlabeled data points into groups or clusters. Minimize the sum of distances between datapoints and their assigned clusters.
+# Intuition: Exlusive or "hard" clustering methods, a data can only exist in one of the groups.
+# Type: Unsupervised clustering method
+# Application Areas: Market segmentation, document clustering, image segmentation, image compression, delivery route optimization, targeting clients
 # Implementation Details:
-# Limitations:
+#       => Iterative method, centoid-based algorithm that partitions a dataset into similar groups based on the distance between their centroids.
+#       => Centorids are "cluster centers", selected from either the mean or the median of all the points wihtin the cluster
+#       => k is the number of clusters/centroids. Large K: signifies smaller clusters with greater detail && Small K: larger clusters with less detail
+#       => Usually Euclidean distance is chosen as the distance metric.
+#       => Maximization step computes the mean of all points for each cluster adn re-assigns the centorids accordingly. 
+#       => Repeat the steps until reached convergence or maximum number of iteration is achieved.
+#       => The more compact and distinct each cluster is, the better.
+#       => Evaluation metric: calculating sum of squared errors (SSE) of each point to its closest centroid evaluates the quality of the cluster
+#               assignments by measuring the total variation within each cluster. 
+# Stepwise:
+#   (1) Choose the number of clusters, k, that you want to create.
+#   (2) Initialize k cluster centroids randomly.
+#   (3) Assign each data point to the nearest centroid, creating k clusters.
+#   (4) Recalculate the centroids as the mean of all data points in each cluster.
+#   (5) Repeat steps 3 and 4 until convergence (centroids no longer change significantly) or for a specified number of iterations.
+# Inertia >> minimization of the intracluster distance, lower sum is better
+# Dunn Index >> minimum intercluster distance and maximum intraluster distance. High interclusters proves how different each cluster is.
+# Limitations: High dependence on the input parameters, significant outlier impact, does poorly when data contains many variations and highly dimensional.
 # ===========================================================================================================================================
